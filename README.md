b165cc94ab62d79edd500578b554c5ff5bc80261
Luiz Pestana <luiz.pestana@gmail.com>
2020-11-05 18:08:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+bentewee-app
---
+++ main.js
@@ -0,0 +1,53 @@
+const {app, shell, BrowserWindow} = require('electron');
+const path = require('path');
+
+let pluginName
+switch (process.platform) {
+  case 'win32':
+    pluginName = './plugins/pepflashplayer.dll';
+    break;
+  case 'darwin':
+    pluginName = './plugins/PepperFlashPlayer.plugin';
+    break;
+  case 'linux':
+    pluginName = './plugins/libpepflashplayer.so';
+    break;
+}
+app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName));
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      plugins: true,
+      webviewTag: true,
+      devTools: false,
+    }
+  });
+
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+}
+
+app.whenReady().then(() => {
+  console.log(process.versions);
+  createWindow();
+  
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('web-contents-created', (e, contents) => {
+  if (contents.getType() == 'webview') {
+    contents.on('new-window', (e, url) => {
+      e.preventDefault();
+      shell.openExternal(url);
+    });
+  }
+});
