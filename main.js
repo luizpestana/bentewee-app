f627ace20e9949ab996ee78f3c9253a5c3427181
Luiz Pestana <luiz.pestana@gmail.com>
2020-11-11 13:19:07 -0300

    Debug

--- main.js
+++ main.js
@@ -20,6 +20,11 @@ else {
   });
 }
 
+const isPackaged = ((
+  process.mainModule &&
+  process.mainModule.filename.indexOf('app.asar') !== -1
+) || (process.argv.filter(a => a.indexOf('app.asar') !== -1).length > 0));
+
 let pluginName;
 switch (process.platform) {
   case 'win32':
@@ -32,7 +37,12 @@ switch (process.platform) {
     pluginName = './plugins/libpepflashplayer.so';
     break;
 }
-app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName));
+if (isPackaged) {
+  pluginName = path.join(__dirname, '../../', pluginName);
+} else {
+  pluginName = path.join(__dirname, pluginName);
+}
+app.commandLine.appendSwitch('ppapi-flash-path', pluginName);
 
 function createWindow () {
   if (mainWindow) {
@@ -43,6 +53,7 @@ function createWindow () {
     width: 1280,
     height: 720,
     icon: `${__dirname}/build/icon.png`,
+    title: __dirname,
     webPreferences: {
       plugins: true,
       webviewTag: true,
